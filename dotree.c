1a42448a7fe570c6b60d7944403f59fa0499156f
Alennir <polo92270@hotmail.fr>
2017-01-13 11:19:03 +0100

    Makefile

---
+++ dotree.c
@@ -0,0 +1,5 @@
+#include "decisionTree.h"
+
+t_tree      *doTree(t_info *info) {
+    return (NULL);
+}
---
+++ main.c
@@ -0,0 +1,11 @@
+#include "decisionTree.h"
+
+int main(void){
+    t_info  *info;
+    t_tree  *tree;
+
+    info = NULL;
+    doinfo(info);
+    //tree = doTree(&info);
+	return (0);
+}
